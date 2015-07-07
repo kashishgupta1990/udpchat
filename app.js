14fcfe0d0fb6f005a9ba20a482c84f263c08f894
Kashish Gupta <kashishgupta1990@yahoo.com>
2015-07-08 00:04:30 +0530

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var argv = require('optimist').argv,
+    dgram = require('dgram'),
+    app = dgram.createSocket('udp4');
+
+if (argv.server) {
+    app.on('listening', function () {
+        var address = app.address();
+        console.log('UDP Server Listen on ' + address.address + ':' + address.port);
+    });
+    app.on('error', function (err) {
+        console.log('Server: ', err);
+    });
+    app.on('message', function (message, remote) {
+        console.log('From:' + remote.address + ':' + remote.port + ': ' + message);
+    });
+    app.bind(argv.server);
+}
+else if (argv.send) {
+    console.log(argv);
+
+    var message = new Buffer(argv._.join(' '));
+    var hostPort = argv.send.split(':');
+    var host = hostPort[0];
+    var port = hostPort[1];
+    app.send(message, 0, message.length, port, host, function (err, byte) {
+        if (err) {
+            console.log('Client: ', err);
+        } else {
+            app.close();
+        }
+    });
+} else {
+    console.log('Incorrect Command');
+    console.log('---- Start Server ----');
+    console.log('udpchat --server PORT');
+    console.log('---- Send Msg ----');
+    console.log('udpchat --send ADDRESS:PORT Hello World');
+}
